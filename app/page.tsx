45570191008f23f3d18bc7a1bb9395adbbd469e4
protimab <protimabanerjee@gmail.com>
2024-03-21 18:46:36 -0400

    initial

---
+++ app/page.tsx
@@ -0,0 +1,18 @@
+import Image from 'next/image';
+export default function Home() {
+  return (
+      <div className="text-5xl text-red-300  text-center font-reenie font-semibold h-screen flex items-center " >
+       <div className= "w-screen text-center"> Hi, my name is Protima!
+        </div>
+        <div style={{ position: 'absolute', top: '35%', right: '18%'}}>
+          <img src='heart.jpg' alt="Description" width='130' height='130' />
+        </div>
+        <div>
+          <img src='jiji.png' alt="Description" width='400' height='400' />
+        </div>
+        <div style={{ position: 'absolute', top: '55%', right: '50%'}}>
+          <img src='cat.gif' alt="Description" width='300' height='300' />
+        </div>
+      </div>
+  );
+}
---
+++ backend/main.py
@@ -0,0 +1,127 @@
+from flask import Flask, request, jsonify
+from flask_cors import CORS
+import sqlite3
+
+app = Flask(__name__)
+CORS(app)
+DB_FILE = 'backend/database.db'
+
+def create_tables():
+    conn = sqlite3.connect(DB_FILE)
+    c = conn.cursor()
+
+    # create user table
+    c.execute('''CREATE TABLE IF NOT EXISTS users (
+                    userID INTEGER PRIMARY KEY AUTOINCREMENT,
+                    username TEXT,
+                    email TEXT, 
+                    join_date DATE, 
+                    profile_picture TEXT, 
+                    bio TEXT
+                )''')
+    
+    # create books table
+    c.execute(''' CREATE TABLE IF NOT EXISTS books (
+                    bookID INTEGER PRIMARY KEY,
+                    authorID INTEGER,
+                    publication_date DATE,
+                    genreID INTEGER,
+                    synopsis TEXT,
+                    FOREIGN KEY (authorID) REFERENCES authors (authorID), 
+                    FOREIGN KEY (genreID) REFERENCES genres (genreID)
+              )''')
+    
+    # create reviews table
+    c.execute(''' CREATE TABLE IF NOT EXISTS reviews (
+                    reviewID TEXT PRIMARY KEY,
+                    userID INTEGER,
+                    bookID INTEGER,
+                    rating TEXT, 
+                    review TEXT, 
+                    review_date DATE,
+                    FOREIGN KEY (userID) REFERENCES users (userID), 
+                    FOREIGN KEY (bookID) REFERENCES books (bookID)
+              )''')
+    
+    # create user library table
+    c.execute(''' CREATE TABLE IF NOT EXISTS user_library (
+                   userID INTEGER,
+                   bookID INTEGER,
+                   has_read TEXT,
+                   PRIMARY KEY (userID, bookID),
+                   FOREIGN KEY (userID) REFERENCES users (userID), 
+                   FOREIGN KEY (bookID) REFERENCES books (bookID)
+              )''')
+    
+    # create genres table
+    c.execute(''' CREATE TABLE IF NOT EXISTS genres (
+                   genreID INTEGER PRIMARY KEY, 
+                   genre_name TEXT
+              )''')
+    
+    # create authors table
+    c.execute(''' CREATE TABLE IF NOT EXISTS authors (
+                   authorID INTEGER PRIMARY KEY, 
+                   author_name TEXT
+              )''')
+    
+    conn.commit()
+    conn.close()
+
+@app.route('/api/users', methods=['GET'])
+def get_users():
+    conn = sqlite3.connect(DB_FILE)
+    c = conn.cursor()
+    c.execute('SELECT * FROM users')
+    users = c.fetchall()
+    conn.close()
+    return jsonify(users)
+
+@app.route('/api/books', methods=['GET'])
+def get_books():
+    conn = sqlite3.connect(DB_FILE)
+    c = conn.cursor()
+    c.execute('SELECT * FROM books')
+    books = c.fetchall()
+    conn.close()
+    return jsonify(books)
+
+@app.route('/api/reviews', methods=['GET'])
+def get_reviews():
+    conn = sqlite3.connect(DB_FILE)
+    c = conn.cursor()
+    c.execute('SELECT * FROM reviews')
+    reviews = c.fetchall()
+    conn.close()
+    return jsonify(reviews)
+
+@app.route('/api/user_library', methods=['GET'])
+def get_user_library():
+    conn = sqlite3.connect(DB_FILE)
+    c = conn.cursor()
+    c.execute('SELECT * FROM user_library')
+    user_library = c.fetchall()
+    conn.close()
+    return jsonify(user_library)
+
+@app.route('/api/genres', methods=['GET'])
+def get_genres():
+    conn = sqlite3.connect(DB_FILE)
+    c = conn.cursor()
+    c.execute('SELECT * FROM genres')
+    genres = c.fetchall()
+    conn.close()
+    return jsonify(genres)
+
+@app.route('/api/authors', methods=['GET'])
+def get_authors():
+    conn = sqlite3.connect(DB_FILE)
+    c = conn.cursor()
+    c.execute('SELECT * FROM authors')
+    authors = c.fetchall()
+    conn.close()
+    return jsonify(authors)
+
+if __name__ == '__main__':
+    create_tables()
+    app.run(debug=True, port=5001)
