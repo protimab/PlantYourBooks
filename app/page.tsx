45fc22914fb081b984472fb9dfb56d2705550931
protimab <protimabanerjee@gmail.com>
2024-03-21 19:21:14 -0400

    interfaces

--- app/page.tsx
+++ app/page.tsx
@@ -1,18 +1,81 @@
-import Image from 'next/image';
-export default function Home() {
-  return (
-      <div className="text-5xl text-red-300  text-center font-reenie font-semibold h-screen flex items-center " >
-       <div className= "w-screen text-center"> Hi, my name is Protima!
-        </div>
-        <div style={{ position: 'absolute', top: '35%', right: '18%'}}>
-          <img src='heart.jpg' alt="Description" width='130' height='130' />
-        </div>
-        <div>
-          <img src='jiji.png' alt="Description" width='400' height='400' />
-        </div>   
-        <div style={{ position: 'absolute', top: '55%', right: '50%'}}>
-          <img src='cat.gif' alt="Description" width='300' height='300' />
-        </div>
-      </div>
-  );
+'use client'
+
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+
+interface Users {
+  userID: number;
+  username: string;
+  email: string;
+  join_date: string;
+  profile_picture: string;
+  bio: string;
 }
+
+interface Books {
+  bookID: number;
+  authorID: number;
+  publication_date: string;
+  genreID: number;
+  synposis: string;
+}
+
+interface Reviews {
+  reviewID: number;
+  userID: number;
+  bookID: number;
+  rating: string;
+  review: string;
+  review_date: string; 
+}
+
+interface UserLibrary {
+  userID: number;
+  bookID: number;
+  has_read: string;
+}
+
+interface Genres {
+  genreID: number;
+  genre_name: string;
+}
+
+interface Authors {
+  authorID: number;
+  author_name: string;
+}
+
+export default function Home() {
+  const [users, setUsers] = useState<Users[]>([]);
+  const [books, setBooks] = useState<Books[]>([]);
+  const [reviews, setReviews] = useState<Reviews[]>([]);
+  const [userLibrary, setUserLibrary] = useState<UserLibrary[]>([]);
+  const [genres, setGenres] = useState<Genres[]>([]);
+  const [authors, setAuthors] = useState<Authors[]>([]);
+
+  useEffect(() => {
+    fetchUsers();
+  }, []);
+
+  const fetchUsers = async () => {
+    try {
+      const resp = await axios.get<Users[]>('http://localhost:5001/api/users');
+      const mappedUsers: Users[] = resp.data.map((usersData: any) => ({
+        userID: usersData[0],
+        username: usersData[1],
+        email: usersData[2],
+        join_date: usersData[3],
+        profile_picture: usersData[4],
+        bio: usersData[5],
+      }));
+      setUsers(mappedUsers);
+    } catch (error) {
+      console.error('Error fetching users:', error);
+    }
+  };
+
+
+
+
+
+} //Home
